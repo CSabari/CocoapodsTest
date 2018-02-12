55552c4348c9f64256ad1c3c73c537302afae100
New <Sabari.mano@gmail.com>
2018-02-12 19:00:52 +0530

    CocopodsTest

--- CocoapodsTest/ViewController.swift
+++ CocoapodsTest/ViewController.swift
@@ -2,24 +2,21 @@
 //  ViewController.swift
 //  CocoapodsTest
 //
-//  Created by TRESELLE on 12/02/18.
+//  Created by SABA on 12/02/18.
 //  Copyright © 2018 com.ios.sample. All rights reserved.
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
